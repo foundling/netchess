342613dfe6cbd0365fae104d105c0dffa15fa828
Alex Ramsdell <alexramsdell@gmail.com>
2016-01-17 16:35:28 -0800

    add rule engine and a knight move validator

--- pieces.js
+++ pieces.js
@@ -1,28 +1,56 @@
+// utility funcs
 var log = console.log;
+
+// pieces 
+var Piece = function(name, position){
+  this.name = name;
+  this.position = position;
+  this.alive = true;
+  this.next = function() {
+    // use rule engine to get next possible moves
+  };
+}; 
+
+// ruleEngine: lays down the possible moves for each piece
+var RuleEngine = function() {
+  this.validators = {
+    knight : {
+      lShape: function(startPos, endPos) {
+        var deltaX = Math.abs(endPos[0] - startPos[0]);
+        var deltaY = Math.abs(endPos[1] - startPos[1]);
+        var hypotenuse = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2));
+        return hypotenuse === Math.sqrt(5);
+      }
+    },
+  };
+};
+
+// gameEngine
+var GameEngine = function() {
+
+};
 
 var Board = function(height, width) {
   this._board = new Array(height*width);
   this.height = height;
   this.width = width;
-  this.posToIndex = function(x,y) {
-    return width  * (x - 1) + 
-           height * (y - 1); 
-  }
+};
+
+Board.prototype.posToIndex = function(position) {
+  var x = position[0];
+  var y = position[1];
+  return this.width  * (x - 1) + 
+         this.height * (y - 1); 
 };
 
 Board.prototype.get = function(position) {
-  var x = position[0];
-  var y = position[1];
-  var targetIndex = this.posToIndex(x,y);
+  var targetIndex = this.posToIndex(position);
   return this._board[targetIndex]; 
 };
 
 Board.prototype.set = function(obj, position) {
-  var x = position[0];
-  var y = position[1];
-  var targetIndex = this.posToIndex(x,y);
+  var targetIndex = this.posToIndex(position);
   this._board[targetIndex] = obj; 
-
 };
 
 var Piece = function(name, position, alive, ruleEngine) {
@@ -38,11 +66,9 @@ Piece.prototype.next = function() {
   return this.ruleEngine.getPossibleMoves(this.name, this.position);
 };
 
-function RuleEngine(board) {
-  
-}
-
-
-var board = new Board(8,8);
-board.set({name:'pawn'}, [8,8]);
-log(board.get([8,8]));
+//var board = new Board(8,8);
+//board.set({name:'pawn'}, [8,8]);
+//log(board.get([8,8]));
+var ruleEngine = new RuleEngine();
+log(ruleEngine.validators.knight.lShape([0,0],[1,2]));
+log(ruleEngine.validators.knight.lShape([1,2],[2,1]));
