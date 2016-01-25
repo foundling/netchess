f36c88e6c62a2402cd6e58c659f96261e70c5fab
Alex Ramsdell <alexramsdell@gmail.com>
2016-01-24 16:57:24 -0800

    added a run method to netchess/main

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 'use strict';
 
 var app = require('./netchess/main');
+
+app.run();
--- netchess/main.js
+++ netchess/main.js
@@ -5,9 +5,17 @@ var Player = require('./player/player');
 var Board = require('./board/board');
 var GameEngine = require('./engine/game_engine');
 
-var board = new Board(8,8);
-var player1 = new Player('player1','near',board);
-var player2 = new Player('player2','far',board);
-var gameEngine = new GameEngine(board,player1,player2);
-board.initBoard();
-gameEngine.refreshDisplay();
+var run = function() {
+
+  var board = new Board(8,8);
+  var player1 = new Player('player1','near',board);
+  var player2 = new Player('player2','far',board);
+  var gameEngine = new GameEngine(board,player1,player2);
+  board.initBoard();
+  gameEngine.refreshDisplay();
+
+};
+
+module.exports = exports = {
+  run: run
+};
