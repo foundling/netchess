e017b5fe093573dcf9ccddb43637cf0d2cd1623b
Alex Ramsdell <alexramsdell@gmail.com>
2016-02-06 14:42:26 -0800

    add board_test

--- netchess/board/board_test.js
+++ netchess/board/board_test.js
@@ -1,13 +1,56 @@
 'use strict';
 
+var mocha = require('mocha');
+var expect = require('chai').expect;
+
 var Board = require('./board');
-var board = new Board(8,8);
 
-var king = {
-  name:'king',
-  position: [0,4]
-};
+describe('board initialization', function(){
+  it('should have H arrays, each of length W', function() {
+    var H = 8,
+        W = 8;
+    var board = new Board(H,W);
+    
+  });
+});
 
-board.setPiece(king);
-console.log(board);
-console.log(board.getPiece([0,4]));
+describe('board.setPiece(piece)', function(){
+
+  it('should place the piece on the board at the x,y coordinates specified by piece.position', function() {
+
+    var board = new Board(8,8);
+    var king = {
+      name:'king',
+      position: [0,4]
+    };
+    var x = king.position[0],
+        y = king.position[1];
+
+    expect(board._board[x][y]).to.eql(undefined);
+    board.setPiece(king); 
+    expect(board._board[x][y]).to.eql(king);
+
+  });
+
+});
+
+describe('board.getPiece(position)', function(){
+
+  it('should return the object at the position specified by position coordinate array', function() {
+
+    var board = new Board(8,8);
+    var king = {
+      name:'king',
+      position: [0,4]
+    };
+    var x = king.position[0],
+        y = king.position[1];
+
+    expect(board.getPiece(king.position)).to.eql(undefined); 
+    board.setPiece(king);
+    expect(board.getPiece(king.position)).to.eql(king); 
+
+  });
+
+});
+
