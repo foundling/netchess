4bc3725eb391945c1be77b11ffaa061b63df0864
Alex Ramsdell <alexramsdell@gmail.com>
2016-02-06 19:07:11 -0800

    drag and drop partially working

--- app.js
+++ app.js
@@ -1,44 +1,4 @@
 'use strict';
-var util = require('util');
 var app = require('./netchess/main');
-var $ = require('jquery');
+var events = require('./netchess/ui/events');
 
-app.run();
-
-var square = document.querySelector('#row0 .square:first-child');
-
-// handlers
-var dragStart = function(ev) {
-  $(this).addClass('move-source');
-  console.log('dragstart');
-};
-var dragEnter = function(ev) {
-  $(this).addClass('move-destination');
-  console.log('dragenter');
-};
-var dragLeave = function(ev) {
-  ev.preventDefault();
-  $(this).removeClass('move-destination');
-  console.log('dragleave');
-};
-var dragOver = function(ev) {
-  ev.preventDefault();
-  ev.dataTransfer.dropEffect = 'move';
-  console.log('dragover');
-  return false;
-};
-var dragOut = function(ev) {
-  ev.preventDefault();
-  console.log('dragover');
-};
-var drop = function(ev) {
-  ev.preventDefault();
-  console.log('dragdrop');
-};
-
-square.addEventListener('dragstart', dragStart);
-square.addEventListener('dragenter', dragEnter);
-square.addEventListener('dragleave', dragLeave);
-square.addEventListener('dragover', dragOver);
-square.addEventListener('dragout', dragOut);
-square.addEventListener('drop', drop);
---
+++ netchess/ui/events.js
@@ -0,0 +1,44 @@
+module.exports = exports = (function() {
+
+  var $ = require('jquery');
+  var squares = $('.square');
+
+  var srcEl = null;
+
+  var dragStart = function(ev) {
+    srcEl = this;
+    ev.dataTransfer.effectAllowed = 'all';
+    $(ev.target).addClass('being-dragged');  
+    console.log('dragstart');
+  };
+
+  var dragEnter = function(ev) {
+    $(this).addClass('over');  
+    console.log('dragenter');
+  };
+
+  var dragOver = function(ev) {
+    ev.preventDefault();
+    console.log('dragover');
+  };
+
+  var dragLeave = function(ev) {
+    $(ev.target).removeClass('over');  
+    console.log('dragleave');
+  };
+
+  var drop = function(ev) {
+    ev.stopPropagation();
+    $(srcEl).removeClass('being-dragged');
+    console.log('drop');
+  };
+
+  squares.each(function(s) {
+    this.addEventListener('dragstart', dragStart);
+    this.addEventListener('dragenter', dragEnter);
+    this.addEventListener('dragover', dragOver);
+    this.addEventListener('dragleave', dragLeave);
+    this.addEventListener('drop', drop);
+  });
+
+}());
